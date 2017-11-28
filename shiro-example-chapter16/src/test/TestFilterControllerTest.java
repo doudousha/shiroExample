e84a88786b1bba3a714e9dac51b8efc912464540
doudousha <dou_dou_sha@sina.com>
2017-11-28 18:58:05 +0800

    '增加properties注入,未完成'

--- shiro-example-chapter16/src/test/TestFilterControllerTest.java
+++ shiro-example-chapter16/src/test/TestFilterControllerTest.java
@@ -4,7 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,11 +21,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
+
+@TestPropertySource(properties  = {"context=test"})
 @ContextConfiguration(locations = {
         "classpath:spring-config.xml",
         "classpath:spring-mvc.xml"
 })
-//
+
 public class TestFilterControllerTest {
 
     @Autowired
@@ -37,11 +41,13 @@ public class TestFilterControllerTest {
                 .addFilter((Filter) wac.getBean("shiroFilter")).build();
     }
 
+
     /**
      * 具体逻辑：
-     *      1.将SysUserFilter添加到上下文中, sysUserFilter 里面获取当前用户添加到request attribute里面
-     *      2.添加CurrentUserMethodArgumentResolver 添加到上下文，将request attribute 里面的值取出来
+     * 1.将SysUserFilter添加到上下文中, sysUserFilter 里面获取当前用户添加到request attribute里面
+     * 2.添加CurrentUserMethodArgumentResolver 添加到上下文，将request attribute 里面的值取出来
      * 测试自定义CurrentUser注解是否讲参数正确注入到method 参数中
+     *
      * @throws Exception
      */
     @Test
